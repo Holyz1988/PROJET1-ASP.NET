35d16997be6efebd7920cb43e3e5cea8d167e154
PC_AMINE\Amine <zeghad.amine@yahoo.fr>
2018-02-01 23:49:16 +0100

    resolution des conflits

--- Simpoll/Simpoll/Controllers/SimpollController.cs
+++ Simpoll/Simpoll/Controllers/SimpollController.cs
@@ -108,46 +108,6 @@ namespace Simpoll.Controllers
         public ActionResult page_creation_sondage(int id)
         {
             return View("page_creation_sondage", id);
-<<<<<<< HEAD
-        }
-=======
-        }               
-
-        public void EnvoiMail(Sondage sondage, Createur createur)
-        {
-            string EmailEnvoyeur = "simpoll.sondage@gmail.com";
-            string password = "Simpoll68@";
-            string objet = "Votre sondage a été crée ";
-
-            string htmlBody = @"<!doctype html>
-                                            <html>
-                                                <body>
-                                                <headers>
-                                                    <img src=""https://image.noelshack.com/fichiers/2018/05/2/1517339614-simpoll.png"" alt=""Logo Simpoll""/>
-                                                </headers>
-                                                <div>                       
-                                                <h2><Merci " + createur.PrenomCreateur + @" d'avoir choisi Simpoll pour créer votre sondage</h2>
-                                                </div>
-                                                <p>Voici vos 3 liens :</p>
-                                                    <ul>
-                                                        <li>" + sondage.UrlPartage + @"</li>
-                                                        <li>" + sondage.UrlResultat + @"</li>
-                                                        <li>" + sondage.UrlSuppression + @"</li>
-                                                    </ul>
-                                                <p>La team Simpoll vous dit à bientôt pour de nouveaux sondage !!</p>                                          
-                                                </body>
-                                                </html>";
-
-            SmtpClient smtp = new SmtpClient
-            {
-                Host = "smtp.gmail.com",
-                Port = 25,
-                EnableSsl = true,
-                DeliveryMethod = SmtpDeliveryMethod.Network,
-                Credentials = new NetworkCredential(EmailEnvoyeur, password),
-            };
->>>>>>> 9ab173da99a4c6c1663f8199bcefcfe512f83bc1
-
-
+        }              
     }
 }
