b18e904df097f8705d1a3173cc0d7f4a85ede847
Amine Zeghad <zeghad.amine@yahoo.fr>
2018-02-05 11:26:55 +0100

    ajout de commentaires dans le PartageController

--- Simpoll/Simpoll/Controllers/PartageController.cs
+++ Simpoll/Simpoll/Controllers/PartageController.cs
@@ -12,12 +12,18 @@ namespace Simpoll.Controllers
 {
     public class PartageController : Controller
     {
-        //Récupère le sondage avec le guid, ce qui permet de sécuriser la page contenant les URLs
+        /* Action qui récupère le sondage avec le guid, ce qui permet de sécuriser la page
+        contenant les URLs
+        */
         public ActionResult page_url(string id)
         {
             return View("page_url", DAL.GetSondageByGuid(id));
         }
 
+        /*Action permettant de récupérer les donnés du sondage la page de vote
+        et afficher la vue de la page avec l'objet SondageAvecReponse, et renvoi
+        vers une page d'erreur si le sondage est désactivé
+        */
         public ActionResult Vote(int? id)
         {
             if (id == null)
@@ -28,6 +34,9 @@ namespace Simpoll.Controllers
             Sondage monSondage;
             List<Reponse> mesReponse;
 
+            /*Le try catch permet d'éviter que le serveur plante si l'utilisateur
+            Tappe n'importe quoi dans l'URL
+            */
             try
             {
                 monSondage = DAL.GetSondageById((int)id);
@@ -38,6 +47,7 @@ namespace Simpoll.Controllers
                 return new HttpNotFoundResult();
             }
 
+            
             if (monSondage.Actif)
             {
                 return View("page_vote", new SondageAvecReponse(monSondage, mesReponse));
@@ -48,6 +58,9 @@ namespace Simpoll.Controllers
             }
         }
 
+        /*Action qui permet de prendre en compte le vote que l'utilisateur à choisi.
+        Cette méthode fonctionne pour les choix multiples ainsi que les choix uniques.
+        */
         public ActionResult VoteSondage(List<int> choixReponse, int? id)
         {
             //Renvoi un 404 si l'id est null
@@ -78,6 +91,10 @@ namespace Simpoll.Controllers
                     return Redirect(string.Format("PageErreurReponse/{0}", id));
                 }
 
+                /*On test si le cookie existe. Si il existe, on renvoi vers la vue
+                 * qui prévient que l'utilisateur a déja voté. Sinon on renvoi vers
+                 la page de résultat et on comptabilise le vote.
+                 */
                 HttpCookie cookie = Request.Cookies["SondageCookie" + id];
                 if (cookie == null)
                 {
@@ -96,17 +113,21 @@ namespace Simpoll.Controllers
                     mesReponse[(int)choix].NbVoteReponse++;
                     DAL.UpdateNombreVoteReponse(mesReponse[(int)choix]);
                 }
-                //On récupère le sondage et on incrémente le nombre de votant max
+
+                //On récupère le sondage et on incrémente le nombre de votant max.
+                //Cette opération est également réalisable directement en base de donnée.
                 monSondage.NbVotant++;
                 DAL.UpdateNombreVotant(monSondage);
-                //SondageAvecReponse monSondageVote = new SondageAvecReponse(monSondage, mesReponse);
 
                 return Redirect(String.Format("Resultat/{0}", id));
             }
 
+            //Sinon on renvoi la vue du sondage désactivé
             return Redirect(String.Format("ConfirmationInactif/{0}", monSondage.Guid));
         }
 
+        /* Action permettant de désactiver le sondage
+        */
         public ActionResult DesactiverSondage(string id)
         {
             if (id == null)
@@ -138,6 +159,10 @@ namespace Simpoll.Controllers
             }
         }
 
+        /* Action permettant l'affichage des résulats. Il suffit de
+        récupérer le SondageComplet en DB et l'envoyer en paramètre 
+        avec la vue résultat.
+        */
         public ActionResult Resultat(int? id)
         {
             if (id == null)
@@ -165,6 +190,9 @@ namespace Simpoll.Controllers
             }
         }
 
+        /*Action qui permet d'afficher la vue qui annonce que l'utilisateur
+        qui ne choisit aucune réponse lors du vote. 
+        */
         public ActionResult PageErreurReponse(int? id)
         {
             if (id == null)
